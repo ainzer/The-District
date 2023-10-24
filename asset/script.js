a2e38b7bb896187baeb021cd577b03f8ea5e3c04
Tony <127862337+ainzer@users.noreply.github.com>
2023-10-24 16:50:16 +0200

    e

--- asset/script.js
+++ asset/script.js
@@ -1,10 +1,1 @@
-// Script pour basculer entre les thèmes
-function toggleTheme(theme) {
-    const body = document.body;
 
-    // Retirer les classes de thème existantes
-    body.classList.remove('light-theme', 'dark-theme');
-
-    // Ajouter la classe du nouveau thème
-    body.classList.add(`${theme}-theme`);
-}
