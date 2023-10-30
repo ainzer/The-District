c6ac3e4dec5f272815ca3246749e3e01a7128e76
Tony <127862337+ainzer@users.noreply.github.com>
2023-10-30 15:05:22 +0100

    30/10/2023

---
+++ asset/JavaScript/accueil.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    // Fonction pour charger le fichier JSON et gérer la recherche
+    function loadAndSearch() {
+        $.getJSON("../asset/the_district.json", function (data) {
+            // Fonction pour gérer la recherche
+            function handleSearch(input) {
+                // Filtrer les plats qui correspondent à la saisie de l'utilisateur
+                var matchingPlats = data.plat.filter(function (plat) {
+                    return plat.libelle.toLowerCase().includes(input.toLowerCase());
+                });
+
+                // Mettre à jour la liste des suggestions visuelles
+                updateSearchResults(matchingPlats);
+            }
+
+            // Fonction pour mettre à jour la liste des suggestions visuelles
+            function updateSearchResults(results) {
+                var resultsContainer = $("#searchResults");
+                resultsContainer.empty(); // Efface les résultats précédents
+
+                // Ajoute les nouveaux résultats à la liste
+                results.forEach(function (plat) {
+                    var resultItem = $("<div>").text(plat.libelle);
+                    resultsContainer.append(resultItem);
+                });
+            }
+
+            // Écouteur d'événements pour la saisie dans la barre de recherche
+            var searchInput = $("#searchInput");
+            searchInput.on('input', function () {
+                var inputValue = searchInput.val();
+                handleSearch(inputValue);
+            });
+        });
+    }
+
+    // Appelle la fonction pour charger le fichier JSON et initialiser la recherche
+    loadAndSearch();
+});
