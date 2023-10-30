bffc83f299a9419f41bea08ce60c37f1ce6d378f
Tony <127862337+ainzer@users.noreply.github.com>
2023-10-30 10:54:49 +0100

    30/10/2023

--- asset/JavaScript/plats.js
+++ asset/JavaScript/plats.js
@@ -1,33 +1,25 @@
 $.getJSON("../asset/the_district.json", function (data) {
     var plats = data.plat;
-  
-    // Récupère le conteneur où tu veux ajouter les cartes de plat
-    var platContainer = $("#plat-container");
+console.log(plats);
+    // Récupérer le conteneur où tu veux ajouter les cartes de plat
+    var platContainer = $("#cartesPlats");
 
-     // Fonction pour générer les cartes
-     function generateCards() {
-        // Boucle sur chaque plat dans le JSON
-        data.plat.forEach(function (plat) {
-            // Vérifie si le plat est actif
-            if (plat.active === "Yes") {
-                // Crée une nouvelle carte avec les données du plat
-                var card = $("<div class='col-md-4 d-flex justify-content-center justify-content-md-end mb-5'>" +
-                "<div class='card zoom-image'>" +
-                "<img src='../image/food/" + plat.image + "' class='plat-img-top card-img' alt='Image du plat'>" +
-                "<div class='card-body'>" +
-                "<h5 class='plat-title'>" + plat.libelle + "</h5>" +
-                "<p class='plat-description'>" + plat.description + "</p>" +
-                "<p class='plat-price'>Prix: " + plat.prix + "€</p>" +
-                "</div>" +
-                "</div>" +
-                "</div>");
+    // Boucle à travers les plats
+    plats.forEach(function (plat) {
+console.log(plat);
+        // Créer une nouvelle carte de plat
+        var newCard = $("<div class='col-md-4 d-flex justify-content-center justify-content-md-end mb-5'>" +
+            "<div class='card zoom-image'>" +
+            "<img src='../image/food/" + plat.image + "' class='plat-img-top card-img' alt='Image du plat'>" +
+            "<div class='card-body'>" +
+            "<h5 class='plat-title'>" + plat.libelle + "</h5>" +
+            "<p class='plat-description'>" + plat.description + "</p>" +
+            "<p class='plat-price'>Prix: " + plat.prix + "€</p>" +
+            "</div>" +
+            "</div>" +
+            "</div>");
 
-                // Ajoute la carte au conteneur approprié
-                $('#plat-container').append(card);
-            }
-        });
-    }
-
-    // Appelle la fonction pour générer les cartes
-    generateCards();
+        // Ajouter la nouvelle carte au conteneur
+        platContainer.append(newCard);
+    });
 });
