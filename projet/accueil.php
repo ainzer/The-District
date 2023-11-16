04dde36adafd6afa6ecdee19e6d6a0b268567466
Tony <127862337+ainzer@users.noreply.github.com>
2023-11-16 12:00:12 +0100

    16/11/2023

--- projet/accueil.php
+++ projet/accueil.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 
-<div class="container-fluid mt-2  d-flex align-items-center justify-content-center">
+<div class="container-fluid mt-2 d-none d-md-block d-flex align-items-center justify-content-center">
     <div class="row">
         <div class="col-md-12">
             <!-- Ajout d'une div pour la vidéo -->
@@ -11,6 +11,19 @@
         </div>
     </div>
 </div>
+
+<div class="container-fluid mt-2 d-block d-md-none d-flex align-items-center justify-content-center">
+    <div class="row">
+        <div class="col-12">
+            <!-- Ajout d'une div pour la vidéo sur les petits écrans -->
+            <div style="width: 100%; height: 200px; margin: auto;">
+                <iframe id="videoFrame" style="width: 100%; height: 100%;" src="../video/video (2160p).mp4" allowfullscreen autoplay></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 
 
